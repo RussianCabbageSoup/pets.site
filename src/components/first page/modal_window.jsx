adf7294b92b0c563974bee8334bc8184a35ebacb
Application_Belchenko <testylya333@gmail.com>
2025-12-09 15:37:40 +0300

    registration

--- src/components/first page/modal_window.jsx
+++ src/components/first page/modal_window.jsx
@@ -12,7 +12,10 @@ function Modal (props) {
     let [error, setError]=useState('none');
     let [message, setMessage]=useState();
 
-    
+    let [user_name, setUser_name] = useState('');
+    let [user_email, setUser_email] = useState('');
+    let [user_phone, setUser_phone] = useState('');
+    let [user_password, setUser_password] = useState('');
 
     function changeData(e) {
 
@@ -99,17 +102,100 @@ function Modal (props) {
     function registation(e) {
         e.preventDefault();
 
+        if (
+            user_name !== '' &&
+            user_email !== '' &&
+            user_phone !== '' &&
+            user_password !== ''
+        ) {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+                "name": user_name,
+                "phone": user_phone,
+                "email": user_email,
+                "password": user_password,
+                "password_confirmation": user_password,
+                "confirm": 1
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+            };
+
+            fetch("https://pets.xn--80ahdri7a.site/api/register", requestOptions)
+            .then(response => response.status)
+            .then(
+                result => {console.log(result)
+                switch (result) {
+                    case 204:
+                        console.log('success');
+                        
+                        break;
+                    case 422:
+                        console.log("ER 422");
+                        
+                        break;
+                }
+        })
+        .catch(error => console.log('error', error));
+        }
     }
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     function reg(event) {
         'use strict'
-    
         const form = document.getElementById('form')
-
         event.preventDefault()
-        event.stopPropagation()
-    
+        event.stopPropagation()    
         form.classList.add('was-validated')    
+
+        if (
+            user_name !== '' &&
+            user_email !== '' &&
+            user_phone !== '' &&
+            user_password !== ''
+        ) {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+                "name": user_name,
+                "phone": user_phone,
+                "email": user_email,
+                "password": user_password,
+                "password_confirmation": user_password,
+                "confirm": 1
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+            };
+
+            fetch("https://pets.xn--80ahdri7a.site/api/register", requestOptions)
+            .then(response => response.status)
+            .then(
+                result => {console.log(result)
+                switch (result) {
+                    case 204:
+                        console.log('success');
+                        
+                        break;
+                    case 422:
+                        console.log("ER 422");
+                        
+                        break;
+                }
+        })
+        .catch(error => console.log('error', error));
+        }
+
+
+
     }
     
     return (
@@ -160,7 +246,7 @@ function Modal (props) {
                     <label htmlFor="validationCustom03" className="form-label"><h5>Имя</h5></label>
                         <div className="input-group col-md-6">
                             <span className="input-group-text" id="addon-wrapping"><img src={icon_human} /></span>
-                            <input type="text" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required />
+                            <input type="text" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required onChange={(e)=>setUser_name(e.target.value)} />
                             <div className="invalid-feedback">
                             Укажите имя
                             </div>
@@ -168,7 +254,7 @@ function Modal (props) {
                     <label htmlFor="validationCustom03" className="form-label"><h5>Адрес электронной почты</h5></label>
                         <div className="input-group col-md-6">
                             <span className="input-group-text" id="addon-wrapping"><img src={icon_mail} /></span>
-                            <input type="email" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required />
+                            <input type="email" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required onChange={(e)=>setUser_email(e.target.value)}/>
                             <div className="invalid-feedback">
                             Укажите почту
                             </div>
@@ -176,7 +262,7 @@ function Modal (props) {
                     <label htmlFor="validationCustom03" className="form-label"><h5>Номер телефона</h5></label>
                         <div className="input-group col-md-6">
                             <span className="input-group-text" id="addon-wrapping"><img src={icon_phone} /></span>
-                            <input type="tel" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required />
+                            <input type="tel" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required onChange={(e)=>setUser_phone(e.target.value)}/>
                             <div className="invalid-feedback">
                             Укажите номер телефона
                             </div>
@@ -184,7 +270,7 @@ function Modal (props) {
                     <label htmlFor="validationCustom03" className="form-label"><h5>Пароль</h5></label>
                         <div className="input-group col-md-6">
                             <span className="input-group-text" id="addon-wrapping"><img src={icon_lock} /></span>
-                            <input type="password" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required />
+                            <input type="password" className="form-control" id="validationCustom03" placeholder='' aria-describedby="addon-wrapping" required onChange={(e)=>setUser_password(e.target.value)}/>
                             <div className="invalid-feedback">
                             укажите пароль
                             </div>
@@ -209,7 +295,7 @@ function Modal (props) {
                             </div>
                         </div>
                         <div className="col-12">
-                            <button className="btn btn-primary" type="submit" onClick={(e)=>registation(e)}>Отправить</button>
+                            <button className="btn btn-primary" type="submit" >Отправить</button>
                         </div>
                     </form>
                 </div>
