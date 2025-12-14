baadb52c454d5bcc2fe5aa5aa57656fa1d3bd6ad
RussianCabbageSoup <testylya333@gmail.com>
2025-12-14 19:00:01 +0300

    user information added

--- src/components/personalAccPage/profileInfo.jsx
+++ src/components/personalAccPage/profileInfo.jsx
@@ -1,4 +1,42 @@
+import { useEffect, useState } from "react";
+
 function ProfileInfo() {
+
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        getInf();
+    }, []);
+
+    function getInf(){
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", `Bearer ${localStorage.token}`);
+
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+        };
+
+        fetch("https://pets.xn--80ahdri7a.site/api/users", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log("API Response:", result);
+
+                const user = result;
+                const userData = {
+                    id: user.id,
+                    name: user.name,
+                    phone: user.phone,
+                    email: user.email,
+                    registrationDate: user.registrationDate,
+                    ordersCount: user.ordersCount
+                }
+
+                setUserData(userData);
+            })
+    }
+
     return ( 
         <div>
             <div>
@@ -11,8 +49,8 @@ function ProfileInfo() {
                     </div>
                     </div>
                     <div className="col-md-10">
-                    <h1 className="h3 mb-1">Иван Петров</h1>
-                    <p className="mb-0">Пользователь с <span id="registrationDays">15</span> апреля 2024 года</p>
+                    <h1 className="h3 mb-1">{userData.name}</h1>
+                    <p className="mb-0">Пользователь с <span id="registrationDays">15</span>{userData.registrationDate}</p>
                     <p className="mb-0 opacity-75" id="daysSinceRegistration" />
                     </div>
                 </div>
@@ -28,8 +66,8 @@ function ProfileInfo() {
                         <h5 className="card-title"><i className="bi bi-info-circle text-primary me-2" />Информация о пользователе</h5>
                         <ul className="list-group list-group-flush">
                         <li className="list-group-item d-flex justify-content-between">
-                            <span>Зарегистрирован:</span>
-                            <span id="registrationDateDisplay">15 апреля 2024</span>
+                            <span>Зарегистрирован: </span>
+                            <span id="registrationDateDisplay">{userData.registrationDate}</span>
                         </li>
                         <li className="list-group-item d-flex justify-content-between">
                             <span>Дней на сайте:</span>
@@ -37,7 +75,7 @@ function ProfileInfo() {
                         </li>
                         <li className="list-group-item d-flex justify-content-between">
                             <span>Объявлений:</span>
-                            <span className="badge bg-success" id="adsCount">3</span>
+                            <span className="badge bg-success" id="adsCount">{userData.ordersCount}</span>
                         </li>
                         </ul>
                     </div>
@@ -50,7 +88,7 @@ function ProfileInfo() {
                     <div className="card-body">
                         <div className="mb-3">
                         <label htmlFor="currentEmail" className="form-label">Текущий email</label>
-                        <input type="email" className="form-control" id="currentEmail" defaultValue="ivan.petrov@example.com" readOnly />
+                        <input type="email" className="form-control" id="currentEmail" defaultValue={userData.email} readOnly />
                         </div>
                         <div className="mb-3">
                         <label htmlFor="newEmail" className="form-label">Новый email</label>
@@ -69,7 +107,7 @@ function ProfileInfo() {
                     <div className="card-body">
                         <div className="mb-3">
                         <label htmlFor="currentPhone" className="form-label">Текущий телефон</label>
-                        <input type="tel" className="form-control" id="currentPhone" defaultValue="+7 (900) 123-45-67" readOnly />
+                        <input type="tel" className="form-control" id="currentPhone" defaultValue={userData.phone} readOnly />
                         </div>
                         <div className="mb-3">
                         <label htmlFor="newPhone" className="form-label">Новый телефон</label>
@@ -108,7 +146,6 @@ function ProfileInfo() {
                 </div>
             </div>
             </div>
-
         </div>
     );
 }
